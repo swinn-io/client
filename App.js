1e461d06a0012f284e2fc67250fb37028b4bdcaa
Burak <burakalbayrak@live.com>
2020-07-28 20:58:33 +0300

    login with api server

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import {makeRedirectUri, useAuthRequest} from 'expo-auth-session';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+    authorizationEndpoint: 'http://192.168.1.103/login',
+    tokenEndpoint: 'http://192.168.1.103/oauth/token',
+    revocationEndpoint: 'http://192.168.1.103/oauth/revoke',
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: '',
+            scopes: [],
+            redirectUri: makeRedirectUri({
+                native: 'exp://redirect',
+                useProxy: false
+            }),
+        },
+        discovery
+    );
+
+    React.useEffect(() => {
+        if(response !== null) {
+            if (response?.type === 'success') {
+                const {code} = response.params;
+            }
+        }
+    }, [response]);
+
+    return (
+        <View style={styles.container}>
+            <Text>Ping Pong Application</Text>
+            <Button
+                disabled={!request}
+                title="Login"
+                onPress={() => {
+                    promptAsync().then((r) => console.log(r));
+                }}
+            />
+            {response && <Text>Hello {response.params.callback.name}!</Text>}
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
