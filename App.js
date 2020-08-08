3f6e3a318307ad4c340b0f7b79ab7b6a29e1d0a3
ikidnapmyself <burakalbayrak@live.com>
2020-08-08 13:20:40 +0300

    Action sheet

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import {makeRedirectUri, useAuthRequest} from 'expo-auth-session';
-import {StyleSheet, Text} from 'react-native';
-import { Body, Button, Container, Header, Icon, Left, Right, Tab, TabHeading, Tabs, Title, ScrollableTab, Subtitle } from 'native-base';
+import {StyleSheet} from 'react-native';
+import { ActionSheet, Body, Button, Container, Header, Icon, Left, Right, Root, Tab, TabHeading, Tabs, Title, ScrollableTab, Subtitle } from 'native-base';
 import LoginTab from "./tabs/LoginTab";
 import PingedTab from "./tabs/PingedTab";
 import ScanTab from "./tabs/ScanTab";
@@ -39,39 +39,79 @@ export default function App() {
         }
     }, [response]);
 
+    let BUTTONS = ["Settings", "Profile", "Mark as seen", "Cancel"];
+    let DESTRUCTIVE_INDEX = 2;
+    let CANCEL_INDEX = 3;
+
     return (
         <Container>
-            <Header hasTabs>
-                <Left />
-                <Body>
-                    <Title>Ping Pong</Title>
-                    <Subtitle>Last Ping 4 min.</Subtitle>
-                </Body>
-                <Right>
-                    <Button transparent>
-                        <Icon name='dots-horizontal' type="MaterialCommunityIcons" />
-                    </Button>
-                </Right>
-            </Header>
-            <Tabs renderTabBar={()=> <ScrollableTab />}>
-                <Tab heading={ <TabHeading><Icon name="login" type="MaterialCommunityIcons" /></TabHeading> }>
-                    <LoginTab />
-                </Tab>
-                <Tab heading={ <TabHeading><Icon name="signal-cellular-2" type="MaterialCommunityIcons" /></TabHeading> }>
-                    <PingedTab />
-                </Tab>
-                <Tab heading={ <TabHeading><Icon name="qrcode-scan" type="MaterialCommunityIcons" /></TabHeading> }>
-                    <ScanTab />
-                </Tab>
-                <Tab heading={ <TabHeading><Icon name="account-circle" type="MaterialCommunityIcons" /></TabHeading> }>
-                    <ProfileTab />
-                </Tab>
-            </Tabs>
-            <StatusBar style="dark"/>
+            <Root>
+                <Header hasTabs>
+                    <Left>
+
+                    </Left>
+                    <Body>
+                        <Title>Ping Pong</Title>
+                        <Subtitle>Last Ping 4 min.</Subtitle>
+                    </Body>
+                    <Right>
+                        <Button transparent onPress={() =>
+                            ActionSheet.show(
+                                {
+                                    options: BUTTONS,
+                                    cancelButtonIndex: CANCEL_INDEX,
+                                    destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                                    title: "More Actions"
+                                },
+                                async buttonIndex => {
+                                    switch (buttonIndex) {
+                                        case 0:
+
+                                            break;
+                                        case 1:
+
+                                            break;
+                                        case 2:
+
+                                            break;
+                                        case 3:
+
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                            )}
+                        >
+                            <Icon name='dots-horizontal' type="MaterialCommunityIcons" style={styles.more} />
+                        </Button>
+                    </Right>
+                </Header>
+                <Tabs renderTabBar={()=> <ScrollableTab />}>
+                    <Tab heading={ <TabHeading><Icon name="login" type="MaterialCommunityIcons" /></TabHeading> }>
+                        <LoginTab />
+                    </Tab>
+                    <Tab heading={ <TabHeading><Icon name="signal-cellular-2" type="MaterialCommunityIcons" /></TabHeading> }>
+                        <PingedTab />
+                    </Tab>
+                    <Tab heading={ <TabHeading><Icon name="qrcode-scan" type="MaterialCommunityIcons" /></TabHeading> }>
+                        <ScanTab />
+                    </Tab>
+                    <Tab heading={ <TabHeading><Icon name="account-circle" type="MaterialCommunityIcons" /></TabHeading> }>
+                        <ProfileTab />
+                    </Tab>
+                </Tabs>
+                <StatusBar style="dark"/>
+            </Root>
         </Container>
     );
 }
 
 const styles = StyleSheet.create({
-
+    more: {
+        color: 'black'
+    },
+    sos: {
+        color: 'red'
+    }
 });
