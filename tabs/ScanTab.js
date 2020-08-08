53fc759197631c176cf4a798a926fe20e14ef460
ikidnapmyself <burakalbayrak@live.com>
2020-08-08 18:43:27 +0300

    qr code display and scanner

--- tabs/ScanTab.js
+++ tabs/ScanTab.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Text } from 'native-base';
-import { View, StyleSheet } from 'react-native';
+import {Body, Button, Content, Icon, Left, ListItem, Right, Separator, Switch, Text} from 'native-base';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import QRCode from "react-native-qrcode-svg";
 
 export default function ScanTab() {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [isEnabled, setIsEnabled] = useState(true);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     useEffect(() => {
         (async () => {
@@ -15,8 +18,8 @@ export default function ScanTab() {
     }, []);
 
     const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        alert(`${data}`);
+        setScanned(data);
+        setIsEnabled(false);
     };
 
     if (hasPermission === null) {
@@ -26,19 +29,70 @@ export default function ScanTab() {
         return <Text>No access to camera</Text>;
     }
 
+    let section = () => {
+        let screenWidth = Dimensions.get("window").width;
+        let logoFromFile = require('../assets/favicon.png');
+        return isEnabled ?
+            (
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+                    style={StyleSheet.absoluteFillObject}
+                />
+            )
+            :
+            (
+                <QRCode
+                    value="Address URI to ping current user"
+                    size={screenWidth-70}
+                    logo={logoFromFile}
+                    logoSize={50}
+                    style={{
+                        alignSelf: 'stretch',
+                        textAlign: 'center',
+                    }}
+                />
+            )
+    }
+
     return (
-        <View
-            style={{
+        <Content style={{
+            flex: 1,
+            flexDirection: 'column',
+        }}>
+            <ListItem style={{marginLeft: 0}}>
+                <Body>
+                    <Text>Scan Mode</Text>
+                </Body>
+                <Right>
+                    <Switch value={isEnabled} onValueChange={toggleSwitch} />
+                </Right>
+            </ListItem>
+            {scanned &&
+                <ListItem style={{marginLeft: 0}} onPress={() => setScanned(false)}>
+                    <Body>
+                        <Text>Scan Again</Text>
+                    </Body>
+                </ListItem>
+            }
+            <Text>{scanned}</Text>
+            <View style={{
+                marginLeft: 30,
+                marginRight: 30,
+                borderWidth: 5,
+                borderColor: '#cccccc',
                 flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
+                aspectRatio: 1,
             }}>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-                style={StyleSheet.absoluteFillObject}
-            />
-            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-        </View>
+                {section(scanned)}
+            </View>
+        </Content>
     );
 }
+
+
+const styles = StyleSheet.create({
+    QRCode: {
+        flex: 1,
+    },
+});
