b0c2cc442a34f53b46eb93ba1654d831b3477afa
Baris Dincer <barisdincer@sabanciuniv.edu>
2021-02-18 18:50:58 +0100

    SocketService Improvements

--- services/socketService.js
+++ services/socketService.js
@@ -55,25 +55,42 @@ const SocketService = {
                 console.log('Socket connected', echo.socketId());
             });
 
+            echo.connector.socket.on('connect_timeout', (timeout) => {
+                console.log('Connection timeout', timeout);
+            });
+
+            echo.connector.socket.on('connect_error', (error) => {
+                console.log('Connection error', error);
+            });
+
             echo.connector.socket.on('subscription_error', (channel, data) => {
                 console.log('Channel subscription error: ' + channel, data);
             });
 
-            echo.connector.socket.on('disconnect', function(){
+            echo.connector.socket.on('disconnect', function(reason){
+                console.log("Disconnection reason", reason);
+                if (reason === 'io server disconnect') {
+                    // the disconnection was initiated by the server, you need to reconnect manually
+                    echo.connector.socket.connect();
+                }
+                // else the socket will automatically try to reconnect
                 console.log('Socket disconnected');
             });
             
             echo.connector.socket.on('reconnecting', function(attemptNumber){
                 console.log('Socket reconnecting', attemptNumber);
-                if(attemptNumber == 3){
-                    console.log("Attempting new connection");
-                    echo.connector.socket.connect();
-                }
             });
 
             echo.connector.socket.on('reconnected', () => {
                 console.log('Socket reconnected',  echo.socketId());
             });
+
+            echo.connector.socket.on('reconnect_failed', () => {
+                //Fired when the client couldn’t reconnect within reconnectionAttempts.
+                console.log('Socket reconnection failed. Trying again...');
+                echo.connector.socket.connect();
+            });
+            
 
         } catch (error) {
             throw error.message;
