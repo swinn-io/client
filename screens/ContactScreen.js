5c19919a6fb633bc1a6ba6bc7029cfd6a4bbce34
Baris Dincer <barisdincer@sabanciuniv.edu>
2021-03-19 00:43:36 +0100

    QR Reader Support and Contact Stack has been introduced

---
+++ screens/ContactScreen.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Container, Content, Button, Text, List, ListItem,
+    Left, Right, Icon, Thumbnail, 
+    Body, Spinner, Fab, View }  from 'native-base';
+import { CustomHeader } from '../components/common'
+
+import constants from '../constants/constants';
+import fetchJson from '../services/fetchJson';
+
+import { MessageContext } from '../services/store/messageStore';
+import { EchoContext } from '../services/store/echoStore';
+import { AuthContext } from '../services/store/authStore';
+
+import { isEmpty } from '../services/helperFunctions';
+
+export default function ContactScreen (props) {
+
+    const [contacts, setContacts] = useState([]);
+    const [user, setUser] = useContext(AuthContext);
+    const userid = user.user.id;
+    
+
+    useEffect(() => {
+        fetchContacts();
+    }, [])
+
+    const fetchContacts = async () => {
+        try {
+            let response = await fetchJson.GET(constants.getAllContacts());
+            let contactsFromResponse = response.data;
+            setContacts(contactsFromResponse);
+        } catch (error) {
+            console.log("Contact List Get Error", error.message);
+        }
+    }
+
+    const openQRReader = () => {
+        props.navigation.navigate("QRReader");
+    }
+
+    return (
+        <Container>
+          <CustomHeader threadTitle={"My Contacts"} props={props}/>
+          <Content>
+            {
+                (contacts).map((contact) => {
+                    
+                    let username = contact.attributes.name;
+                    let userId = contact.attributes.source.id;
+                    return (
+                        <ListItem
+                            key = { userId }
+                            label = { username }
+                            value = { userId }
+                        >
+                            <Body>
+                                <Text>{username}</Text>
+                            </Body>
+                        </ListItem>
+                    )
+                })
+            }
+          </Content>
+            <Fab
+                direction="up"
+                containerStyle={{}}
+                style={{backgroundColor: '#5067FF'}}
+                position="bottomRight"
+                onPress={() => openQRReader()}
+            >
+                <Icon name="add"/>
+            </Fab>
+        </Container>
+    );
+
+       
+}
